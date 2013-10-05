18942b3ada3e6718d451f07bbf747aef5a82820d
Mickaẽl Canévet <mickael.canevet@camptocamp.com>
2013-10-05 07:38:08 +0200

    Add new type freebox_configuration_connection to manage connection configuration

--- manifests/configuration/connection.pp
+++ manifests/configuration/connection.pp
@@ -10,4 +10,14 @@ class freebox::configuration::connection(
   $adblock             = undef,
   $allow_token_request = undef,
 ) {
+  freebox_configuration_connection { $::clientcert:
+    params => {
+      ping                => $ping,
+      remote_access       => $remote_access,
+      remote_access_port  => $remote_access_port,
+      wol                 => $wol,
+      adblock             => $adblock,
+      allow_token_request => $allow_token_request,
+    },
+  }
 }
